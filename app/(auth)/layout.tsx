5c7e700a19f61bc3fbc5c58646e1ee640fe18ac6
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2025-06-16 14:40:57 -0300

    feat: implemented auth and storage with firebase configuration

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,14 @@
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
 import { ReactNode } from 'react'
 
-const AuthLayout = ({ children }: { children: ReactNode}) => {
+const AuthLayout = async ({ children }: { children: ReactNode}) => {
+  const isUserAutheticated = await isAuthenticated();
+
+  if (isUserAutheticated) redirect('/');
+
   return (
-    <div>{children}</div>
+    <div className="auth-layout">{children}</div>
   )
 }
 
---
+++ firebase/client.ts
@@ -0,0 +1,18 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdGOy9vxdowone8q57n2olUXwuAukIu0E",
+  authDomain: "ai-interview-f5ed0.firebaseapp.com",
+  projectId: "ai-interview-f5ed0",
+  storageBucket: "ai-interview-f5ed0.firebasestorage.app",
+  messagingSenderId: "813428975765",
+  appId: "1:813428975765:web:559c8696d2c02b5793d39c",
+  measurementId: "G-HZ26C9JGH0"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
